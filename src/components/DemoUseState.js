751db41ba2008473fec0fc349ec6d064f06c8e64
llckumadevelop <kumauser01@kuma.ink>
2024-07-04 09:28:08 +0700

    create component

---
+++ src/components/DemoUseState.js
@@ -0,0 +1,12 @@
+import React, {useState} from "react";
+
+function DemoUseState(props) {
+  let [state,setState] = useState({})
+  return (
+    <div>
+      <p>hello</p>
+    </div>
+  );
+}
+
+export default DemoUseState;
